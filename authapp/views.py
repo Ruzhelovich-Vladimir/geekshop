983cdaf09516929198590c07d8b7cf283420b32a
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2021-01-17 13:35:43 +0600

    lesson-1: Generate mail to activation key. STEP#1

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,5 @@
+from django.core.mail import send_mail
+from django.conf import settings
 from django.shortcuts import render, HttpResponseRedirect
 from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 from django.contrib import auth
@@ -5,18 +7,35 @@ from django.urls import reverse
 
 from authapp.forms import ShopUserEditForm
 
+
+def send_verify_mail(user):
+    verify_link = reverse('auth:verify', args=[
+                          user.email, user.activation_key])  # Генерация ссылки
+
+    subject = f'подтверждение учётной записи {user.username}'
+
+    message = f'Для подтверждения учетной записи {user.username} на портале \
+        {settings.DOMAIN_NAME} перейдите по ссылке: \n{settings.DOMAIN_NAME}{verify_link}'
+
+    return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
+
+def verify(request, email, actitation_key):
+    pass
+
+
 def login(request):
     title = 'вход'
-    
+
     login_form = ShopUserLoginForm(data=request.POST or None)
-    
+
     next = request.GET['next'] if 'next' in request.GET.keys() else ''
     #print('next', next)
-    
+
     if request.method == 'POST' and login_form.is_valid():
         username = request.POST['username']
         password = request.POST['password']
-        
+
         user = auth.authenticate(username=username, password=password)
         if user and user.is_active:
             auth.login(request, user)
@@ -27,47 +46,52 @@ def login(request):
                 return HttpResponseRedirect(reverse('main'))
 
     content = {
-        'title': title, 
-        'login_form': login_form, 
+        'title': title,
+        'login_form': login_form,
         'next': next
     }
-    
+
     return render(request, 'authapp/login.html', content)
 
 
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main'))
-    
+
 
 def register(request):
     title = 'регистрация'
-    
+
     if request.method == 'POST':
         register_form = ShopUserRegisterForm(request.POST, request.FILES)
-    
+
         if register_form.is_valid():
-            register_form.save()
+            user = register_form.save()
+            if send_verify_mail(user):
+                print("success")
+            else:
+                print("failed")
             return HttpResponseRedirect(reverse('auth:login'))
     else:
         register_form = ShopUserRegisterForm()
-    
+
     content = {'title': title, 'register_form': register_form}
-    
+
     return render(request, 'authapp/register.html', content)
-    
-    
+
+
 def edit(request):
     title = 'редактирование'
-    
+
     if request.method == 'POST':
-        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        edit_form = ShopUserEditForm(
+            request.POST, request.FILES, instance=request.user)
         if edit_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
-    
+
     content = {'title': title, 'edit_form': edit_form}
-    
+
     return render(request, 'authapp/edit.html', content)
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -134,3 +134,22 @@ MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'authapp.ShopUser'
 
 LOGIN_URL = '/auth/login/'
+
+"""
+Локальная отправка почты
+"""
+DOMAIN_NAME = 'http://localhost:8000'
+
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = '2525'
+EMAIL_HOST_USER = 'django@geekshop.local'
+EMAIL_HOST_PASSWORD = 'geekshop'
+EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+
+# вариант python -m smtpd -n -c DebuggingServer localhost:2525 - стартует п
+# EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = None, None
+# вариант логирования сообщений почты в виде файлов вместо отправки
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'tmp/email-messages/'
