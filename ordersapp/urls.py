b250cf6f25c3df862558eee8429e59ec36f76914
Владимир Ружелович <42509323+Ruzhelovich-Vladimir@users.noreply.github.com>
2021-01-27 23:34:49 +0600

    Lesson 3: Готовое домашнее задание (#4)
    * lesson-3: STEP#1 Working with orders
    
    * lesson-3: STEP#2 Admin orders list
    
    * lesson-3: STEP#3 Admin orders list

---
+++ ordersapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+# from django.urls import re_path
+
+import ordersapp.views as ordersapp
+
+app_name = 'ordersapp'
+
+urlpatterns = [
+    path('', ordersapp.OrderList.as_view(), name='orders'),
+    path('create/', ordersapp.OrderCreate.as_view(), name='order_create'),
+    path('update/<pk>/', ordersapp.OrderUpdate.as_view(), name='order_update'),
+    path('delete/<pk>/', ordersapp.OrderDelete.as_view(), name='order_delete'),
+    path('detail/<pk>/', ordersapp.OrderRead.as_view(), name='order_read'),
+    path('forming/<pk>/', ordersapp.order_forming_complete,
+         name='order_forming_complete'),
+]
