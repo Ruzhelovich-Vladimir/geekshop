7443972143827061a8ee30a851f6ac04c1e07b9d
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2021-02-04 22:58:30 +0600

    lesson-6: install django-extensions

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -82,6 +82,7 @@ if DEBUG:
         'debug_toolbar.panels.redirects.RedirectsPanel',
         'debug_toolbar.panels.profiling.ProfilingPanel',
         'template_profiler_panel.panels.template.TemplateProfilerPanel',
+        'django_extensions',
     ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
