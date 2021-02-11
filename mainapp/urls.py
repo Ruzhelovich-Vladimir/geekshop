1588ed4cf95eea7b9efb9440da0d1103876f4597
OmskShip <Ruzhelovich.Vladimir@gmail.com>
2021-02-11 23:57:16 +0600

    lesson-7:Step1 - cheching site

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 import mainapp.views as mainapp
+from django.views.decorators.cache import cache_page
+
 
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.products, name='index'),
     path('category/<int:pk>/', mainapp.products, name='category'),
-    path('category/<int:pk>/page/<int:page>/', mainapp.products, name='page'),
+    path('category/<int:pk>/page/<int:page>/',
+         cache_page(3600)(mainapp.products), name='page'),
     path('product/<int:pk>/', mainapp.product, name='product'),
 ]
